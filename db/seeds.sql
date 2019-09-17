12a7fe67457c66fffde77f07b9374e8bac88f04e
Nina Shin <48607661+ninashin66@users.noreply.github.com>
2019-09-16 19:54:25 -0600

    app, db, conig setup -orm

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Chik-Fil-A', 0);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('In-N-Out', 1);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('The Habit', 0);
---
+++ server.js
@@ -0,0 +1,3 @@
+var express = require("express");
+var hdbs = requires("express-handlebars");
+var mysql = require("mysql");
